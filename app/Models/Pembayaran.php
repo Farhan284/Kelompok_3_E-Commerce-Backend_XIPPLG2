443203a08b259daf72afcfeec77df0608ad2b0f3
Naufall12345 <156062096+Naufall12345@users.noreply.github.com>
2025-06-03 11:19:44 +0700

    commit

--- app/Models/Pembayaran.php
+++ app/Models/Pembayaran.php
@@ -1,1 +1,32 @@
-.
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pembayaran extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pembayaran';
+    protected $fillable = [
+        'pembayaran_id',
+        'id_pesanan',
+        'id_pelanggan',
+        'metode_pembayaran',
+        'status_pembayaran',
+        'tanggal_pembayaran',
+        'total_pembayaran'
+    ];
+
+    public function pesanan()
+    {
+        return $this->belongsTo(Pesanan::class, 'id_pesanan');
+    }
+
+    public function pelanggan()
+    {
+        return $this->belongsTo(Pelanggan::class, 'id_pelanggan');
+    }
+}
